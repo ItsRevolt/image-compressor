e5e5477d1fca98a14051eac91b924f9b91e4aefd
Mario <revoltsgfx@gmail.com>
2020-03-19 21:05:41 -0400

    Init

---
+++ index.js
@@ -0,0 +1,43 @@
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+const sharp = require('sharp');
+var AWS = require('aws-sdk');
+var s3Bucket = new AWS.S3({
+    endpoint: 'nyc3.digitaloceanspaces.com',
+    region: 'nyc3',
+    params: {
+        Bucket: 'tely-bucket'
+    }
+});
+var nanoid = require('nanoid');
+
+async function s3Upload(input, key) {
+    var params = {
+        Key: key,
+        Body: input,
+        ContentType: 'image/jpg',
+        ACL: 'public-read'
+    };
+    let uploaded = await s3Bucket.upload(params).promise()
+    if (uploaded) {
+        return 'https://tely-bucket.nyc3.cdn.digitaloceanspaces.com/' + key
+    }
+}
+
+
+async function compress(imgBuffer) {
+    var image = Buffer.from(imgBuffer, 'base64');
+    const data = await sharp(image).resize(140, 140).jpeg().toBuffer()
+    return data;
+}
+
+exports.upload = async (req, res) => {
+    console.log(req.file)
+    var compressed = await compress(req.body.file.replace(/^data:image\/\w+;base64,/, ""))
+    var upload = await s3Upload(compressed, req.body.key + '-' + nanoid())
+    res.status(200).send(upload);
+};
