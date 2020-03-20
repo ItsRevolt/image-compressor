e6aad4e4e37fe5c020d0e2dd956aa50384ff676f
Mario <revoltsgfx@gmail.com>
2020-03-19 22:55:51 -0400

    Update image-compressor.js

--- api/image-compressor.js
+++ api/image-compressor.js
@@ -36,7 +36,7 @@ module.exports = async (req, res) => {
   const { body } = req;
   console.log(body);
   var compressed = await compress(
-    body.file.replace(/^data:image\/\w+;base64,/, "")
+    body.file.data.replace(/^data:image\/\w+;base64,/, "")
   );
   var upload = await s3Upload(compressed, body.key + "-" + nanoid());
   return res.status(200).send(upload);
