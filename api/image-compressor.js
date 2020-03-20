30a4a8bfb6a8aaddc8bd95237e54834e0524b1c7
Mario <revoltsgfx@gmail.com>
2020-03-19 22:15:22 -0400

    switch to zeit

---
+++ api/image-compressor.js
@@ -0,0 +1,41 @@
+const sharp = require("sharp");
+var AWS = require("aws-sdk");
+var s3Bucket = new AWS.S3({
+  endpoint: "nyc3.digitaloceanspaces.com",
+  region: "nyc3",
+  params: {
+    Bucket: "tely-bucket"
+  }
+});
+var nanoid = require("nanoid");
+import { NowRequest, NowResponse } from "@now/node";
+
+async function s3Upload(input, key) {
+  var params = {
+    Key: key,
+    Body: input,
+    ContentType: "image/jpg",
+    ACL: "public-read"
+  };
+  let uploaded = await s3Bucket.upload(params).promise();
+  if (uploaded) {
+    return "https://tely-bucket.nyc3.cdn.digitaloceanspaces.com/" + key;
+  }
+}
+
+async function compress(imgBuffer) {
+  var image = Buffer.from(imgBuffer, "base64");
+  const data = await sharp(image)
+    .resize(140, 140)
+    .jpeg()
+    .toBuffer();
+  return data;
+}
+
+export default async (req, res) => {
+  var compressed = await compress(
+    req.body.file.replace(/^data:image\/\w+;base64,/, "")
+  );
+  var upload = await s3Upload(compressed, req.body.key + "-" + nanoid());
+  return res.json(upload);
+};
